e9f906159550078362f7e1d1722f494ff62278b5
kelvinwbsantos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-10-13 19:02:06 -0300

    feat: create enrollment dto

--- src/enrollments/dto/create-enrollment.dto.ts
+++ src/enrollments/dto/create-enrollment.dto.ts
@@ -1,1 +1,11 @@
-export class CreateEnrollmentDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsUUID } from "class-validator";
+
+export class CreateEnrollmentDto {
+    @ApiProperty({
+        description: 'Certificate ID user want to enroll',
+        example: 'a1b2c3d4-e5f6-7890-1234-567890abcdef',
+    })
+    @IsUUID()
+    certificateId: string;
+}
