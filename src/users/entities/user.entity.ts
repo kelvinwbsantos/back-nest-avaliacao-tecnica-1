c0a23b2afa32f5f424240d17ec3083f450204543
Kelvin W. B. Santos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-11-05 21:43:44 -0300

    feat: Adiciona registro minímo sem convite

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -18,22 +18,22 @@ export class User {
     @Column({ length: 14, unique: true, nullable: false })
     cpf: string;
 
-    @Column({ length: 11 })
+    @Column({ length: 11, nullable: true })
     phonenumber: string;
 
-    @Column({ length: 8 })
+    @Column({ length: 8, nullable: true })
     cep: string;
 
-    @Column({ length: 2 })
+    @Column({ length: 2, nullable: true })
     uf: string;
 
-    @Column({ length: 30 })
+    @Column({ length: 30, nullable: true })
     city: string;
 
-    @Column({ length: 40 })
+    @Column({ length: 40, nullable: true })
     neighborhood: string;
 
-    @Column({ length: 100 })
+    @Column({ length: 100, nullable: true })
     street: string;
 
     @Column({ length: 100, nullable: false })
