4c2f26a67564eb8001ef9eecee0a926a595f094f
Kelvin W. B. Santos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-11-10 22:02:04 -0300

    fix: certificate manytoone constraint

--- src/certificates/entities/certificate.entity.ts
+++ src/certificates/entities/certificate.entity.ts
@@ -1,6 +1,6 @@
 import { Certification } from "src/certifications/entities/certification.entity";
 import { User } from "src/users/entities/user.entity";
-import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Certificate {
@@ -19,11 +19,11 @@ export class Certificate {
     @CreateDateColumn()
     createdAt: Date;
 
-    @OneToOne(() => User)
+    @ManyToOne(() => User)
     @JoinColumn({ name: "userId"})
     user: User;
 
-    @OneToOne(() => Certification)
+    @ManyToOne(() => Certification)
     @JoinColumn({ name: "certificationId"})
     certification: Certification;
 }
